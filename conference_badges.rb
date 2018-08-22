439579f77f078832eeec9b6b99200fce05aa6ad1
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 00:13:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_messages = []
+  names.each do | name |
+   return badge_messages <<  badge_maker(name)
+  end
+end
+
