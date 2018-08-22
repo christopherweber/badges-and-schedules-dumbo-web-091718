db1fba48589f601a7cf7155fdd45f0ca5a83a042
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 00:39:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,8 @@ end
 def assign_rooms(speakers)
   counter = 0
   rooms = (1..7)
-    if rooms.count > 7
-    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    while rooms.count > 7
+    
+    return "Hello, #{name}! You'll be assigned to room #{index+1}!"
   
 end
